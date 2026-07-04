c351f5cacdf1807f36f6b064016cb1ab4f21d885
Shreya Sharma <shreyasharmaa516@gmail.com>
2026-07-04 17:43:13 +0530

    feat: initialize React frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header/Header.jsx";
+import Home from "./pages/Home/Home.jsx";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  );
+}
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <h1>SentinelAI</h1>;
+}
